52b499ca7260012a4f7096956b0827c7a45f044e
Davas2710 <davidvasil@icloud.com>
2026-09-25 22:05:40 +0200

    Enhance diagnostic display in dk-support-planner.js
    Updated the display message and styling for the diagnostic information.

--- dk-support-planner.js
+++ dk-support-planner.js
@@ -1,6 +1,19 @@
 (function(){
+var rows=document.querySelectorAll('tr.call-village');
+var units=document.querySelectorAll('.unit_checkbox');
+var cells=document.querySelectorAll('td[data-unit]');
 var x=document.createElement('div');
-x.style.cssText='position:fixed;top:10px;left:10px;right:10px;z-index:999999;background:#ff0;border:3px solid #000;padding:15px;font-size:18px;color:#000;';
-x.innerHTML='MOBILNÝ JS TEST FUNGUJE';
+
+x.style.cssText=
+'position:fixed;top:10px;left:10px;right:10px;z-index:999999;'+
+'background:#fff;border:3px solid #000;padding:15px;font-size:16px;color:#000;';
+
+x.innerHTML=
+'<b>DK DIAGNOSTIKA</b><br>'+
+'Dediny: '+rows.length+'<br>'+
+'Checkboxy jednotiek: '+units.length+'<br>'+
+'Bunky jednotiek: '+cells.length+'<br>'+
+'URL screen: '+location.href;
+
 document.body.appendChild(x);
 })();
