87d59a16df78887e53bde6d82ee18ddd04cc8102
Davas2710 <davidvasil@icloud.com>
2026-09-25 22:14:45 +0200

    Refactor support planner to collect input element data

--- dk-support-planner.js
+++ dk-support-planner.js
@@ -1,6 +1,17 @@
 (function(){
-var x=document.createElement('div');
-x.style.cssText='position:fixed;top:5px;left:5px;right:5px;bottom:5px;z-index:999999;background:white;color:black;border:3px solid black;padding:10px;font-size:13px;overflow:auto;';
-x.textContent=document.body.innerText.slice(0,10000);
+var a=[];
+document.querySelectorAll('input,td,tr,table').forEach(function(e){
+ var s=e.tagName+
+ ' id='+e.id+
+ ' class='+e.className+
+ ' name='+e.getAttribute('name')+
+ ' data-unit='+e.getAttribute('data-unit')+
+ ' data-count='+e.getAttribute('data-count')+
+ ' data-title='+e.getAttribute('data-title');
+ if(s.length<500)a.push(s);
+});
+var x=document.createElement('pre');
+x.style.cssText='position:fixed;top:0;left:0;right:0;bottom:0;z-index:999999;background:#fff;color:#000;padding:10px;overflow:auto;font-size:11px;white-space:pre-wrap';
+x.textContent=a.join('\\n');
 document.body.appendChild(x);
 })();
